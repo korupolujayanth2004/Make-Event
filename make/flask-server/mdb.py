73f62deae8ceb209bf6fdca287cca38a9e655d32
korupolujayanth2004 <jayanthkorupolu2004@gmail.com>
2025-06-07 20:31:00 +0530

    Main changes

---
+++ make/flask-server/mdb.py
@@ -0,0 +1,86 @@
+import flask
+import pymongo
+from flask import Flask
+from pymongo import MongoClient
+mongo_url = "mongodb+srv://jayanthkorupolu2004:lEkJ9w1rfFEh2Kl5@myclue.iwpj3rb.mongodb.net/?retryWrites=true&w=majority&appName=myclue"
+user = MongoClient(mongo_url)
+
+db = user['user_data']
+
+user_name = db['user_name']
+
+def create_user(username, email, password, location,fullname):
+  un = user_name.find_one({'username': username})
+  em = user_name.find_one({'email': email})
+  if un is None and em is None:
+    user_name.insert_one({
+        'fullname': fullname,
+        'username': username,
+        'email': email,
+        'password': password,
+        'location': location,
+        'hc_bal': 20,
+        'hst': [],
+        'hlst': []
+    })
+    return "user created"
+  else:
+    return "user already exists"
+
+
+def login_retri(username):
+  un = user_name.find_one({'username': username})
+  if un is None:
+    return "create an account"  # url to be inserted to go to main page
+  else:
+    return un['password']
+
+
+def append_locdata(latitude, longitude):
+      un=user_name.find_one({'username': 'default'})
+      un['location_data'].append([latitude, longitude])
+
+      return un['location_data']
+
+
+def inchc_bal(username, hc_count):
+  un = user_name.find_one({'username': username})
+  if un is not None:
+    un['hc_bal'] = hc_count + un['hc_bal']
+
+
+def dechc_bal(username, hc_count):
+  un = user_name.find_one({'username': username})
+  if un is not None:
+    un['hc_bal'] = un['hc_bal'] - hc_count
+
+
+def get_usrdata(username):
+  un = user_name.find_one({'username': username})
+  if un is not None:
+    return [un['hc_bal'], un['username'], un['email'], un['location'], un['fullname']]
+  else:
+    return []
+
+
+def hf_data(username, title, description, location, hc_pay):    #extra functionality
+  selected_users = user_name.find({'location': location})
+  for user in selected_users:
+    user['hlst'].append({
+        'username': username,
+        'title': title,
+        'description': description,
+        'location': location,
+        'hc_pay': hc_pay
+    })
+  return []
+
+def available_helps():
+  un = user_name.find_one({'username': 'default'})
+  return un['hlst']
+
+
+def list_help(title,description,location,balance,username):
+  filter= {'username': 'default'}
+  update={'$push':{'hlst': [ title,description,location,balance,username]}}
+  user_name.update_one(filter,update)
---
+++ make/flask-server/server.py
@@ -0,0 +1,92 @@
+'''from flask import Flask
+app=Flask(__name__)
+@app.route("/members")
+def members():
+    return {"members":["member1","member2","member3"]}
+
+if __name__=="__main__":
+    app.run(debug=True)'''
+from flask import Flask, render_template, request, redirect, url_for
+from mdb import create_user, login_retri, get_usrdata, hf_data, append_locdata,available_helps,list_help
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def fun1():
+  return render_template('index.html')
+
+@app.route("/geo_data" , methods = ['POST'])
+def index():
+    lat = request.form['latitude']
+    lon = request.form['longitude']
+    all_locations = append_locdata(lat,lon)
+    return render_template("index.html", lati = lat , longi = lon, all_locations = all_locations)
+
+@app.route('/account_create', methods=['POST'])
+def create2_user():
+  fullname = str(request.form['fullname'])
+  username = str(request.form['username'])
+  email = str(request.form['email'])
+  password = str(request.form['password'])
+  location = str(request.form['location'])
+  status = create_user(username, email, password, location, fullname)
+  li=get_usrdata(username)
+  if status == 'user created':
+      return render_template('profile.html', username=username, balance=li[0],email=li[2],location=li[3],fullname=li[4])
+
+@app.route('/signup')
+def signup():
+   return render_template('signup.html')
+
+@app.route('/login')
+def login():
+   return render_template('login.html')
+
+
+@app.route('/account_login', methods=['POST'])
+def login1():
+  username = str(request.form['username'])
+  password_g = str(request.form['password'])    #password_g is given password
+  status= login_retri(username)
+  li=get_usrdata(username)
+  if status != 'create an account':
+      password_db = status
+      if password_g == password_db:
+          return render_template('profile.html', username=username, balance=li[0],email=li[2],location=li[3],fullname=li[4])
+      else:
+          return render_template('index.html')
+            
+@app.route('/help/<username>')
+def help(username):
+   return render_template('request.html' , username=username)
+
+@app.route('/avhelps')
+def avhelps():
+   li=available_helps()
+   return render_template('map.html',li=li)
+
+@app.route('/get_data', methods=['POST'])
+def get_usrdata1():
+  username = str(request.form['username'])
+  return get_usrdata(username)
+
+@app.route('/view_help')
+def view_help():
+   return render_template('helpview.html')
+
+@app.route('/store_helps', methods=['POST'])   
+def store_helps():
+  username = str(request.form['username'])
+  title = str(request.form['title'])
+  description = str(request.form['description'])
+  location = str(request.form['location'])
+  hc_pay = str(request.form['hc_pay'])
+  list_help( title, description, location, hc_pay,username)
+  li=available_helps()
+  return render_template('map.html', username=username,li=li)
+
+
+if __name__ == "__main__":
+  app.run(host='0.0.0.0', debug=True)
